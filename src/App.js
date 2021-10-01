af4170f623b5d440cac9f18695e6a0aeed47078d
Showmick Kar <showmick12@gmail.com>
2021-10-02 00:58:12 +0600

    Update App/.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,16 @@ const App = () => {
   const [courseGpa, setCourseGpa] = useState([]);
   const [page, setPage] = useState("Home");
 
+  useEffect(() => {
+    if (page !== "Home") return;
+    setUser({
+      currentCgpa: 0.0,
+      coursesCompleted: 0,
+      numOfCourses: 4,
+    });
+    setCourseGpa([]);
+  }, [page]);
+
   return page === "Home" ? (
     <Home user={user} setUser={setUser} setPage={setPage} />
   ) : page === "Courses" ? (
@@ -25,9 +35,15 @@ const App = () => {
       setPage={setPage}
     />
   ) : page === "ErrorHandling" ? (
-    <ErrorHandling setPage={setPage}/>
+    <ErrorHandling setPage={setPage} />
   ) : (
-    <CGPA user={user} courseGpa={courseGpa} setPage={setPage} />
+    <CGPA
+      user={user}
+      courseGpa={courseGpa}
+      setCourseGpa={setCourseGpa}
+      page={page}
+      setPage={setPage}
+    />
   );
 };
 
